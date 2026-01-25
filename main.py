2528ff4e72de95584f01a8371bb3bdf9077969c3
Tim Benson <tim.benson@deutschebahn.com>
2026-01-25 12:53:03 +0100

    feat: argument parser

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from argparse import ArgumentParser, Namespace
+from logging import Logger
+import logging
+
+
 def main():
-    print("Hello from digsinet-v2!")
+    
+    logger: Logger = logging.getLogger("digsinet-v2")
+    
+    parser: ArgumentParser = ArgumentParser(
+        description="digsinet-v2 command line interface",
+    )
+
+    parser.add_argument("--debug", help="Enable debug logging", action="store_true")
+    parser.add_argument("--stop", help="Stop all siblings and cleanup containers", action="store_true")
+    parser.add_argument("--config", help="Path to configuration file, defaults to digsinet.py", type=str, default="digsinet.yml")
+
+    arguments: Namespace = parser.parse_args()
+
 
 
 if __name__ == "__main__":
