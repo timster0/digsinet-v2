47179456ff7d526824b3486fe10c27139b9800eb
Tim Benson <tim.benson@deutschebahn.com>
2026-01-31 10:25:13 +0100

    wip: controller ABC

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from logging import Logger
+from multiprocessing import Process
+
+from config.settings import Settings
 
 class Controller(ABC):
     """
@@ -51,17 +55,23 @@ class Controller(ABC):
 
         self._name = name
 
-    def __init__(self):
+    def __init__(
+        self,
+        logger: Logger,
+        config: Settings,
+        real_topology_definition: dict,
+    ):
         """
         Initialize the controller.
 
+        Also initializes an event broker for event streaming and communicating with other controllers.
+        
         Args:
             config (dict): contents of configuration file and derived supplemental configuration
                 values.
             real_topology_definition (dict): real network topology definition (e.g., containerlab YAML)
             real_nodes (dict): nodes in the real network
             sibling (str): name of the sibling to create
-            broker (EventBroker): broker for event streaming (e.g. RabbitMQ, Kafka)
 
         Returns:
             None
@@ -69,4 +79,26 @@ class Controller(ABC):
         Raises:
             None
         """
-        pass
+
+        self.logger: Logger = logger
+        self.config: Settings = config
+        self.real_topology_definition: dict = real_topology_definition
+        self.broker = None  # TODO: Initialize event broker here
+
+        self.process: Process = Process(target=self.__run, name="Controller " + self.name)
+        self.process.start()
+        self.logger.info(f"Started controller process for {self.name} with PID {self.process.pid}")
+
+    @abstractmethod
+    def __run(self):
+        """
+        Start the controller with the applications for the assigned sibling / realnet
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
